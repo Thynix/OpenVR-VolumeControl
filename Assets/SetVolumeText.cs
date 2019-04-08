dd4e90d6cf9746337b88a2fd0813ecc763b3d5a9
Steve Dougherty <steve@asksteved.com>
2019-04-08 06:12:23 -0400

    Change volume text only on start and on haptic pulse
    This seems cleaner than updating unnecessarily on every frame; I don't
    actually see a clear difference in the profiler. The canvas may well avoid
    re-rendering text if it hasn't changed. Or maybe it does it every frame
    but it's cheap. Dunno!
    
    The editor UI looks as if an event can be associated
    non-programatically, but I couldn't get that to work.

--- Assets/SetVolumeText.cs
+++ Assets/SetVolumeText.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
 
 // Set the text on the parent object to the volume integer.
 public class SetVolumeText : MonoBehaviour
 {
     private Text output;
     public SystemVolumeLink source;
+    public HapticRack hapticTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
         output = GetComponent<Text>();
+        UpdateText();
+
+        hapticTrigger.onPulse.AddListener(UpdateText);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateText()
     {
         output.text = source.FromScalar().ToString();
     }
